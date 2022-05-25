d94f0524b832718cefa9a275fef68786b3847f59
tanveer <mdtanviralamht731@gmail.com>
2022-05-25 17:55:34 +0600

    login component designed

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,89 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    // signInWithEmailAndPassword(data.email, data.password);
+  };
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="flex justify-center min-h-screen items-center">
+      <div className="shadow-lg rounded-lg w-full text-accent  mx-2 p-[30px] lg:w-96 ">
+        <h2 className="text-center text-[30px] mt-[15px]  mb-[30px]">Login</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="input-box mb-[10px]">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              {...register("email", {
+                required: { value: true, message: "Email is required" },
+                pattern: {
+                  value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                  message: "Please Enter the valid Email",
+                },
+              })}
+              id="email"
+              class="input input-bordered input-secondary w-full "
+            />
+            <label htmlFor="email" className="text-red-400 text-xs">
+              {(errors.email?.type === "required" && errors.email?.message) ||
+                (errors.email?.type === "pattern" && errors.email?.message)}
+            </label>
+          </div>
+          <div className="input-box">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              class="input input-bordered input-secondary w-full "
+              {...register("password", {
+                required: { value: true, message: "Password is required" },
+                minLength: {
+                  value: 6,
+                  message: "Password length must be 6 character ",
+                },
+              })}
+              id="password"
+            />
+            <label htmlFor="password" className="text-red-400 text-xs">
+              {(errors.password?.type === "required" &&
+                errors.password?.message) ||
+                (errors.password?.type === "minLength" &&
+                  errors.password?.message)}
+            </label>
+          </div>
+          <div className="reset mb-[20px]">
+            <Link
+              to="/reset"
+              className="text-sm hover:text-secondary duration-200 ease-in-out "
+            >
+              Forget Password?
+            </Link>
+          </div>
+          <button
+            type="submit"
+            // disabled={loading || googleLoading ? true : false}
+            className="uppercase text-base-100 mb-[15px] bg-secondary w-full rounded-lg p-[15px]"
+          >
+            login
+          </button>
+        </form>
+        <p className="text- text-center mb-[16px]">
+          New to Doctors Portal?
+          <Link to="/signup" className="text-secondary ml-2">
+            Create New Account
+          </Link>
+        </p>
+        <div className="divider mb-[25px]">OR</div>
+        <button className="uppercase border w-full border-secondary text-secondary text-xl p-[15px] rounded-lg">
+          continue with Google
+        </button>
+      </div>
     </div>
   );
 };
