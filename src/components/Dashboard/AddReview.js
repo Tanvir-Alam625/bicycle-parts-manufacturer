19bdb87a73f423898494f9e2f5b35d6c47d9425d
tanveer <mdtanviralamht731@gmail.com>
2022-05-26 13:43:46 +0600

    rating star added

--- src/components/Dashboard/AddReview.js
+++ src/components/Dashboard/AddReview.js
@@ -1,9 +1,57 @@
 import React from "react";
 
 const AddReview = () => {
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const formData = {
+      name: event.target.name.value,
+      img: event.target.img.value,
+      rating: parseInt(event.target.rating.value),
+      description: event.target.description.value,
+    };
+    console.log(formData);
+  };
   return (
-    <div>
-      <h2>Add Review</h2>
+    <div className=" flex flex-col justify-center items-center  pt-12">
+      <div className="shadow-lg rounded-lg w-full text-accent bg-base-100 mx-2 p-[30px] lg:w-96 ">
+        <h2 className="text-center text-[30px] mt-[15px]  mb-[30px]">
+          Sign Up Now
+        </h2>
+        <form onSubmit={handleFormSubmit}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Name"
+            required
+            class="input input-bordered input-secondary w-full  mb-[20px]"
+          />
+          <input
+            type="number"
+            name="rating"
+            min={1}
+            max={5}
+            required
+            placeholder="You Ratings"
+            class="input input-bordered input-secondary w-full mb-[20px] "
+          />
+          <input
+            type="text"
+            name="img"
+            required
+            placeholder="Your Image url"
+            class="input input-bordered input-secondary w-full mb-[20px] "
+          />
+          <textarea
+            class="textarea w-full textarea-secondary mb-[20px]"
+            placeholder="Message"
+            name="description"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-secondary w-full">
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/Home/Review.js
+++ src/components/Home/Review.js
@@ -1,7 +1,8 @@
 import React from "react";
+import ReactStars from "react-rating-stars-component";
 
 const Review = ({ data }) => {
-  const { img, name, description } = data;
+  const { img, name, description, rating } = data;
   return (
     <div class="card lg:max-w-lg bg-base-100 shadow-xl">
       <div class="card-body">
@@ -13,6 +14,12 @@ const Review = ({ data }) => {
           />
           <h2 className="text-2xl ml-2">{name}</h2>
         </div>
+        <ReactStars
+          count={rating}
+          size={24}
+          activeColor="#ffd700"
+          className="text-[#ffd700]"
+        />
         <p className="p-0 m-0" title={description}>
           {description.length > 150
             ? description.slice(0, 150) + "..."
